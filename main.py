7be3ad68a7ddfdcf0cfc8fa4018370238653ef4b
Horst JENS <horstjens@gmail.com>
2021-07-22 11:45:51 +0200

    target_destination, arrows

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # evolution 3d for Simon
 import vpython
-import vpython as vp  # vpython must be installed from vpython.org
+#import vpython as vp  # vpython must be installed from vpython.org
 import random
 
 
@@ -13,23 +13,32 @@ import random
 #    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
 
 class Game:
-    scene1 = vpython.canvas(width=800, height=600, title='Simons wonderful water world')
+    scene1 = vpython.canvas(width=1400, height=800, title='Simons wonderful water world')
     grid_size = 1  # should be 1 until we understand what it does
     fps = 60
     grid_dim = 2  # 1 = 1 cube, 2 = 2x2x2 cubes, 3=3x3x3 cubes...
     cube_to_grid_ratio = 0.95
     max_swimmers = 10
     friction = 0.93
+    show_arrows = False
     show_trail = False
     retain = 30 # for trails
     pps = 15    # for trails (currently not implimented)
+
 
     cubedict = {}
     plantdict = {}
     particledict = {}
     swimmidict = {}
 
-    # TODO: compounds?
+    @classmethod
+    def random_point(cls):
+        """returns a random point inside the universe"""
+        return vpython.vector(
+            random.uniform(-Game.grid_size / 2, Game.grid_dim * Game.grid_size - Game.grid_size / 2),
+            random.uniform(-Game.grid_size / 2, Game.grid_dim * Game.grid_size - Game.grid_size / 2),
+            random.uniform(-Game.grid_size / 2, Game.grid_dim * Game.grid_size - Game.grid_size / 2),
+        )
 
 
 class Plant(vpython.simple_sphere):
@@ -45,7 +54,7 @@ class Plant(vpython.simple_sphere):
         self.spawn_speed = random.uniform(0.4, 1.5)
 
         if "color" not in kwargs or kwargs["color"] is None:
-            self.color = vp.vector(random.random(), random.random(), random.random())
+            self.color = vpython.vector(random.random(), random.random(), random.random())
         if "radius" not in kwargs or kwargs["radius"] is None:
             self.radius = random.random()
 
@@ -109,7 +118,7 @@ class Cube(vpython.box):
             # the more swimmies in me, the redder i become
             total = len(Game.swimmidict)
             mine = self.amount_of_swimmies
-            self.color = vp.vector(mine / total, self.color.y, self.color.z)
+            self.color = vpython.vector(mine / total, self.color.y, self.color.z)
 
 
 class Swimmi(vpython.compound):
@@ -122,18 +131,20 @@ class Swimmi(vpython.compound):
     wing_height = 0.01 # for
     wing_to_cone_length = 1.0   # 1.0
     wing_to_cone_width = 0.4    # 0.4
-    wing_to_cone_root = 0.9     # 0.2
-
+    wing_to_cone_root = 0.2     # 0.2
+    wing_angle = 15
+    max_age = None
 
     def __init__(self, **kwargs):
         # for k, v in kwargs.items():
         if "pos" not in kwargs or kwargs["pos"] is None:
             # if (newpos.x > Game.grid_dim * Game.grid_size - Game.grid_size / 2) or (newpos.x < - Game.grid_size / 2):
-            kwargs["pos"] = vpython.vector(
-                random.uniform(-Game.grid_size / 2, Game.grid_dim * Game.grid_size - Game.grid_size / 2),
-                random.uniform(-Game.grid_size / 2, Game.grid_dim * Game.grid_size - Game.grid_size / 2),
-                random.uniform(-Game.grid_size / 2, Game.grid_dim * Game.grid_size - Game.grid_size / 2),
-            )
+            #kwargs["pos"] = vpython.vector(
+            #    random.uniform(-Game.grid_size / 2, Game.grid_dim * Game.grid_size - Game.grid_size / 2),
+            #    random.uniform(-Game.grid_size / 2, Game.grid_dim * Game.grid_size - Game.grid_size / 2),
+            #    random.uniform(-Game.grid_size / 2, Game.grid_dim * Game.grid_size - Game.grid_size / 2),
+            #)
+            kwargs["pos"] = Game.random_point()
         if "axis" not in kwargs or kwargs["axis"] is None:
             kwargs["axis"] = vpython.norm(vpython.vector.random()) * 0.07
         # overwrite radius with 0.03
@@ -150,11 +161,12 @@ class Swimmi(vpython.compound):
         # create sub-components for compound object
         body = vpython.cone(pos=vpython.vector(0, 0, 0), axis=vpython.vector(Swimmi.cone_length, 0, 0), radius=Swimmi.cone_radius)
         wing1 = vpython.box(pos=vpython.vector(Swimmi.cone_length * Swimmi.wing_to_cone_root , 0, 0), axis=body.axis, size=vpython.vector(Swimmi.cone_length * Swimmi.wing_to_cone_width, Swimmi.cone_length * Swimmi.wing_to_cone_length, Swimmi.wing_height))
-        wing1.rotate(vpython.radians(15), axis=body.axis)
+        wing1.rotate(vpython.radians(Swimmi.wing_angle), axis=body.axis)
         wing2 = vpython.box(pos=vpython.vector(Swimmi.cone_length * Swimmi.wing_to_cone_root , 0, 0), axis=body.axis, size=vpython.vector(Swimmi.cone_length * Swimmi.wing_to_cone_width, Swimmi.cone_length * Swimmi.wing_to_cone_length, Swimmi.wing_height))
-        wing2.rotate(vpython.radians(-15), axis=body.axis)
+        wing2.rotate(vpython.radians(-Swimmi.wing_angle), axis=body.axis)
 
         super().__init__([body, wing1, wing2], **kwargs)
+
 
         print("Ich bin ein Swimmi")
         self.number = Swimmi.number
@@ -162,10 +174,16 @@ class Swimmi(vpython.compound):
         Game.swimmidict[self.number] = self
         self.age = 0
         self.angle = 0
-        self.max_age = random.uniform(500, 500)
+        #self.max_age = random.uniform(500, 500)
         self.speed = random.uniform(Swimmi.min_speed, Swimmi.max_speed)
-        self.nervousness = random.random() + 0.01
-        self.iwanttogothere = vpython.vector.random()
+        self.nervousness = 0.25 # how often per second to change whereiwanttogo 1 = every second, 2= twice per second, 0.1 = all 10 seconds
+        self.target_destination = Game.random_point()
+        self.iwanttogothere = self.target_destination - self.pos
+
+
+    def new_target_destination(self):
+        """selects a random point inside the cube universe"""
+        self.target_destination = Game.random_point()
 
     def update(self):
         # hide/show trail
@@ -180,16 +198,18 @@ class Swimmi(vpython.compound):
         self.reflect()
         self.pos += vpython.norm(self.axis) * self.speed * 1 / Game.fps
         # aging
-        self.age += 1 / Game.fps
-        if self.age > self.max_age:
-            # kill correctly
-            self.visible = False
+        if self.max_age is not None:
+            self.age += 1 / Game.fps
+            if self.age > self.max_age:
+                # kill correctly
+                self.visible = False
         # rotating randomly
         self.pitchaxis = vpython.cross(self.axis, self.up)
         rot_axis = random.choice([self.axis, self.up, self.pitchaxis])
         self.angle += random.uniform(-0.1, 0.1)
-        if random.random() < self.nervousness:
-            self.iwanttogothere = vpython.vector.random()
+        if random.random() < self.nervousness / Game.fps:
+            self.target_destination = Game.random_point()
+        self.iwanttogothere = self.target_destination - self.pos # update constantly
         diff_angle = vpython.diff_angle(self.axis, self.iwanttogothere)
         if diff_angle > 0:
             for axis in [self.axis, self.up, self.pitchaxis]:
@@ -232,7 +252,7 @@ class Swimmi(vpython.compound):
 
 def create_widgets():
 
-
+    # ---------- trail control ------------
     vpython.checkbox(bind=toggle_trail, text='show trails for swimmies   ')  # text to right of checkbox
     #vpython.wtext(text="pps:")
     #vpython.slider(bind=change_pps, min=0.5, max=30)
@@ -242,7 +262,23 @@ def create_widgets():
     vpython.wtext(text="   ")
     Game.retain_text = vpython.wtext(text=Game.retain)
     Game.scene1.append_to_caption('\n\n')
+    # ------ where i want to go arrow control ------
+    vpython.checkbox(bind=toggle_arrows, text="show 'where i want to go' arrows", value=True)
+    Game.scene1.append_to_caption('\n\n')
+
+
 ## --------- functions for widget functionality -------------
+
+def toggle_arrows(cross):
+    Game.show_arrows = cross.checked
+    print("arrows:", Game.show_arrows, cross.checked)
+    for swimmi in Game.swimmidict.values():
+        if Game.show_arrows:
+            swimmi.arrow.start()
+        else:
+
+            swimmi.arrow.stop()
+
 def toggle_trail(cross):
     if not cross.checked:
         # remove all existing trails
@@ -262,30 +298,34 @@ def change_retain(slide):
 
 # ------------- end of widget functions ---------------------
 def create_world():
-    xarrow = vp.arrow(pos=vp.vector(0, 0, 0), axis=vp.vector(1, 0, 0), color=vp.vector(1, 0, 0))  # red
-    yarrow = vp.arrow(pos=vp.vector(0, 0, 0), axis=vp.vector(0, 1, 0), color=vp.vector(0, 1, 0))  # green
-    zarrow = vp.arrow(pos=vp.vector(0, 0, 0), axis=vp.vector(0, 0, 1), color=vp.vector(0, 0, 1))  # blue
-    xlabel = vp.label(pos=xarrow.pos + xarrow.axis, color=xarrow.color, text="x")
-    ylabel = vp.label(pos=yarrow.pos + yarrow.axis, color=yarrow.color, text="y")
-    zlabel = vp.label(pos=zarrow.pos + zarrow.axis, color=zarrow.color, text="z")
+    xarrow = vpython.arrow(pos=vpython.vector(0, 0, 0), axis=vpython.vector(1, 0, 0), color=vpython.vector(1, 0, 0))  # red
+    yarrow = vpython.arrow(pos=vpython.vector(0, 0, 0), axis=vpython.vector(0, 1, 0), color=vpython.vector(0, 1, 0))  # green
+    zarrow = vpython.arrow(pos=vpython.vector(0, 0, 0), axis=vpython.vector(0, 0, 1), color=vpython.vector(0, 0, 1))  # blue
+    xlabel = vpython.label(pos=xarrow.pos + xarrow.axis, color=xarrow.color, text="x")
+    ylabel = vpython.label(pos=yarrow.pos + yarrow.axis, color=yarrow.color, text="y")
+    zlabel = vpython.label(pos=zarrow.pos + zarrow.axis, color=zarrow.color, text="z")
 
     for x in range(Game.max_swimmers):
         Swimmi()
-
+    # arrows
+    for s in Game.swimmidict.values():
+        s.arrow = vpython.attach_arrow(s, "iwanttogothere", scale=1.0, color=vpython.vector(0, 0, 1),
+                         shaftwidth=Swimmi.cone_length / 4)
+        s.arrow.stop()
 
 def create_cubes():
     for x in range(0, Game.grid_dim, Game.grid_size):
         for y in range(0, Game.grid_dim, Game.grid_size):
             for z in range(0, Game.grid_dim, Game.grid_size):
-                Cube(pos=vp.vector(x, y, z),
-                     size=vp.vector(Game.grid_size * Game.cube_to_grid_ratio,
+                Cube(pos=vpython.vector(x, y, z),
+                     size=vpython.vector(Game.grid_size * Game.cube_to_grid_ratio,
                                     Game.grid_size * Game.cube_to_grid_ratio,
                                     Game.grid_size * Game.cube_to_grid_ratio,
                                     ),
-                     color=vp.vector(0.75, 0.75, 0.75),
+                     color=vpython.vector(0.75, 0.75, 0.75),
                      opacity=0.15,
                      )
-                Plant(pos=vp.vector(x, y, z), radius=Game.grid_size * Game.cube_to_grid_ratio / 10)
+                Plant(pos=vpython.vector(x, y, z), radius=Game.grid_size * Game.cube_to_grid_ratio / 10)
 
 
 def display():
@@ -298,11 +338,20 @@ def display():
     for cube in Game.cubedict.values():
         cube.change_color()
 
-    # kill dead stuff
-    for mydict in [Game.swimmidict, Game.particledict]:
-        for dead_thing in [item for item in mydict.values() if item.age > item.max_age]:
-            del mydict[dead_thing.number]
-            del dead_thing
+    # kill dead stuff: particles
+    for particili in [item for item in Game.particledict.values() if item.age > item.max_age]:
+        del Game.particledict[particili.number]
+        del particili
+    # kill dead stuff: swimmies
+    if Swimmi.max_age is not None:
+        for swimmi in [item for item in Game.swimmidict.values() if item.age > item.max_age]:
+            del Game.swimmidict[swimmi.number]
+            del swimmi
+    #for mydict in [Game.swimmidict, Game.particledict]:
+    #
+    #    for dead_thing in [item for item in mydict.values() if item.age > item.max_age]:
+    #        del mydict[dead_thing.number]
+    #        del dead_thing
 
 
 def main():
@@ -314,7 +363,7 @@ def main():
 
     # update swimmies
     while True:
-        vp.rate(Game.fps)
+        vpython.rate(Game.fps)
         display()
 
 
